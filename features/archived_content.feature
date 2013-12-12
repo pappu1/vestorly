d202b328a772a58fa097f15f9a624791dc8a37af
SA <pappusagar@gmail.com>
2013-12-12 23:20:21 +0530

    Developed new scenarios

--- features/archived_content.feature
+++ features/archived_content.feature
@@ -8,7 +8,7 @@ Feature: Archived content feature
   @needs_login_data 
   Scenario: From archived content
      And I wait for 30 seconds
-	 Given I am signed in for archived content
+	 Given I am visited forgot password 
 	 Then I visit the your content page for archived content
 	 Then I should see archived content
 	 
--- features/step_definitions/archived_content.rb
+++ features/step_definitions/archived_content.rb
@@ -1,10 +1,9 @@
-When(/^I am signed in for archived content$/) do
-  visit '/sagar'
-  fill_in "username", with: "pappusagar@gmail.com"
-  fill_in "password", with: "sagar@perfect"
-  click_on "Log in to Your Account"
-end
-
+When(/^I am visited forgot password $/)do
+  visit 'http://u153730.sendgrid.org/wf/click?upn=HmG2tc-2BsufsMzRBfPuJGkwOh42h-2FWRYz9iFI3WXZIUrHVxv-2BaFxWfoeUyf1ZJUgr-2FyB1m53B7jnJbTfLyq5d51B7vhSWIVZVoVa0tAPzyOREEMnpmIn5o9NF9flSNi3xa9gUoyaScBFqM-2FrnPxRu4q1xYZbY8lgiTReMgeeNJTerpboUW-2B-2FrL-2BONQA2h9KDdao-2BIj81pOfmQ8Acrx-2BSRfK5afze-2BsmAF3qpR6-2BP5EeYn1urgGctTaRHGsh2aKhez_aunKAZDY4LoBiUwhWmXL1yIb1ejSeWleL2kX-2BtcEsGBFp3AHHpk3J7oAlmOume41Ds3tF4kcyJMdE2VqnkC-2B4EGGgK7fsF6SZ3aQR89avHnHixwtl2vNtI5JRGT6Y6IIqKpUpfXqTVZOdZ4Qyva7RkcMJwKB60q1xGUB7HlHp61bPYA6u3mSt50bTMF5I0Q2ko3hLEiKooywkhVQR2qYTdVh91JhJCzSJ16khtdxpW-2B9QKR2sznDkmpx-2F6rA4cqR'
+  fill_in "new_password", with: "sagar@perfect"
+  click_on "Save Changes"
+  end
+ 
 Then(/^I visit the your content page for archived content$/) do
  click_on "Your Content"
  click_on "Archived Content"
